c4fa1822a1635735afd4ef15ff63c2b3d4f38648
aywey <yeanleyton6@gmail.com>
2025-05-16 15:34:56 -0400

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.js 	modified:   frontend/src/components/navbar/navbar.css 	modified:   frontend/src/components/navbar/navbar.jsx 	new file:   frontend/src/components/sidebar/sidebar.css 	new file:   frontend/src/components/sidebar/sidebar.jsx 	modified:   frontend/src/index.js 	renamed:    frontend/src/App.css -> frontend/src/pages/HomePage.css 	new file:   frontend/src/pages/HomePage.jsx 	new file:   frontend/src/pages/admin.css 	new file:   frontend/src/pages/admin.jsx 	new file:   frontend/src/pages/productos.css 	new file:   frontend/src/pages/productos.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
-import React from 'react';
-import NavbarFerremax from './components/navbar/navbar';
-import HeaderFerremax from './components/header/header';
-import ImgBanner from './components/carousel/imagenesCarousel';
-import FooterFerremax from './components/footer/footer';
-import MediaCard from './components/desarrollo/cards';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AdminPage from './pages/admin';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <header>
-        <HeaderFerremax/>
-        <NavbarFerremax/>
-        <ImgBanner/>
-      </header>
-      <main>
-        <div className='div-card'>
-          <MediaCard/>
-        </div>
-      </main>
-      <footer>
-        <FooterFerremax/>
-      </footer>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   );
 }
 
