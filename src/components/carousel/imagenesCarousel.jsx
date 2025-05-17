4815882e3199dc3b47c96d9149d5af4c0b744d91
edwardvillarroel <ed.villarroel@duocuc.cl>
2025-05-16 20:27:28 -0400

    Revert "cambiosDaniel"
    This reverts commit 30ef2215bb6c2806d47e1800d267fadd8b0e7db1.

--- src/components/carousel/imagenesCarousel.jsx
+++
@@ -1,8 +0,0 @@
-import Image from 'react-bootstrap/Image';
-import './carousel.css'
-
-function ImgBanner() {
-  return <Image src="/imagenes/foto3.jpg" fluid className='img-banner'/>;
-}
-
-export default ImgBanner;
